9c897a4329377d53077d8eceeb496fcf0b7f29f4
Dave Spencer <david.spencerian@gmail.com>
2020-06-09 21:57:30 -0700

    ping

---
+++ ping-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/pi/Projects/Pingomatic/log
+
+
+
+/usr/bin/rrdtool create ping.rrd \
+--step 60 \
+DS:received:GAUGE:120:0:10 \
+DS:max:GAUGE:120:0:1000 \
+		 RRA:MAX:0.5:1:1500 
+
+# $rrdtool update latency_db.rrd --template pl:rtt N:$RETURN_DATA
---
+++ ping-cron.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ping="/bin/ping -c 10 -n -q"
+target="8.8.8.8"
+command="${ping} ${target}"
+out=/tmp/$$
+
+trap "rm -f ${out}" EXIT
+
+${command} > ${out}
+
+received=$(grep transmitted ${out} |  cut --fields=2 -d, | cut --fields=2 -d" ")
+max=$(grep "rtt min/avg/max/mdev" ${out} | cut --fields=6 -d /)
+
+cd /home/pi/Projects/Pingomatic/log
+cp ${out} last.txt
+echo received=${received} >> last.txt
+echo max=${max} >> last.txt
+
+/usr/bin/rrdtool update ping.rrd --template received:max N:${received}:${max}
+
+
+
